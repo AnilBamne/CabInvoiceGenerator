7f1bae2bd1dbc712bd93cddbf20df0b44b9b20a0
AnilBamne <anilbamne20@gmail.com>
2022-12-21 21:43:41 +0530

    Adding:WelcomeMessage

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace CabInvoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to cab invoice generator");
+        }
+    }
+}
